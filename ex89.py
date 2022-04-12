d1b3fd834971176ee1991584573009c1e8d74690
thiago Ribeiro <thiagohsantana@icloud.com>
2022-04-12 14:16:15 -0300

    primeiro commit

---
+++ ex89.py
@@ -0,0 +1,18 @@
+'''Boletim com listas compostas'''
+ficha = list()
+while True:
+    nome = str(input('Nome: '))
+    nota1 = float(input('Nota 1: '))
+    nota2 = float(input('Nota 2: '))
+    média = (nota1 + nota2) / 2
+    ficha.append([nome, [nota1, nota2], média])
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'{"No.":<4}{"NOME":<10}{"MÉDIA":>8}')
+print('-' * 26)
+for i, a in enumerate(ficha):
+    print(f'{i:<4}{a[0]:<10}{a[2]:>8.1f}')
+print('Finalizando...')
+print('-' * 26)
