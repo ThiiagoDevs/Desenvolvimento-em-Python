86b52a349727df06cbc50d498130e8e8866cb07e
thiago Ribeiro <thiagohsantana@icloud.com>
2022-06-26 01:09:26 -0300

    opção de sistemas

---
+++ sistema.py
@@ -0,0 +1,7 @@
+opcao = str(input('Deseja sair do sistema [S/N]'))
+if (opcao == 's'):
+    print('Você saiu do sistema.')
+elif (opcao == 'n'):
+    print('Você continua no sistema.')
+else:
+    print('Opção invalida!')
